11fddb3db383b90be408a9bc36d57adc1af25188
Dustin Falgout <dustin@falgout.us>
2015-01-28 02:11:49 -0600

    start slider script when document is ready AND on action:ajaxify:end event

--- static/lib/lavender.js
+++ static/lib/lavender.js
@@ -168,43 +168,25 @@ $('document').ready(function () {
 		};
 	}());
 	(function () {
-		$(document).ready(function () {
-				if ($('.category-page').length) {
+		function doSlick() {
+				if ($('.subcategories').length) {
 
 					$('.subcategories').slick({
 						dots: true,
 						infinite: true,
 						speed: 300,
 						slidesToShow: 4,
-						slidesToScroll: 4/*,
-						responsive: [
-							{
-								breakpoint: 1024,
-								settings: {
-									slidesToShow: 3,
-									slidesToScroll: 3,
-									infinite: true,
-									dots: true
-								}
-							},
-							{
-								breakpoint: 600,
-								settings: {
-									slidesToShow: 2,
-									slidesToScroll: 2
-								}
-							},
-							{
-								breakpoint: 480,
-								settings: {
-									slidesToShow: 1,
-									slidesToScroll: 1
-								}
-							}
-						]*/
+						slidesToScroll: 4
 					});
 				}
-			});
+		}
+		$(document).ready(function () {
+			doSlick();
+		});
+		$(window).on('action:ajaxify.end', function (ev, data) {
+			doSlick();
+		});
+
 	}());
 });
 
