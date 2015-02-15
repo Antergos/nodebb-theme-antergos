7c05b5753b4c5d45b68a4b1604eb20e8de798027
Dustin Falgout <dustin@falgout.us>
2015-02-15 01:00:49 -0600

    removed too much

--- static/lib/lavender.js
+++ static/lib/lavender.js
@@ -50,7 +50,7 @@ $('document').ready(function () {
 		function resize(fixed) {
 			fixed = parseInt(fixed, 10);
 
-			var container = fixed ? $('.container-fluid') : $('.container-fluid');
+			var container = fixed ? $('.container') : $('.container');
 			container.toggleClass('container-fluid', fixed !== 1).toggleClass('container', fixed === 1);
 			localStorage.setItem('fixed', fixed);
 		}
